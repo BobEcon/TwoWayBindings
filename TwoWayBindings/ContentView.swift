79e1b2925cdd27759a951f0dc640bffc94510d5c
Bob Beachill <bbeachill@icloud.com>
2025-02-10 00:22:51 +0000

    Added two controls wwith binding variables: Toggle & TextField

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pressCount: Int = 0
+    @State private var toggleISON: Bool = true
+    @State private var name: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Text("Press Count: \(pressCount)")
+            
+            Button("Press Me") {
+                pressCount += 1
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
+            TextField("Enter a name", text: $name)
+            Text("Name entered: \(name)")
+            
+            Spacer()
+            
+            Toggle("Toggle is \(toggleISON ? "on" : "off")", isOn: $toggleISON)
+
+            Spacer()
         }
+        .font(.title2)
+        .border(Color.gray)
         .padding()
+       
     }
 }
 
